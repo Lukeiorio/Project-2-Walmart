8ac4b6c2a934dcbe3b315edb3ffbb4d358c9f4b8
Payedas7 <payedas7@outlook.com>
2018-10-30 16:13:33 -0400

    This one to be use

---
+++ updated_schema.sql
@@ -0,0 +1,25 @@
+Drop database project2_db;
+
+CREATE DATABASE project2_db;
+USE project2_db;
+
+-- Create tables for raw data to be loaded into
+CREATE TABLE features_table (
+date date PRIMARY KEY,
+Temperature float,
+Fuel_Price float,
+CPI float,
+unemployment float
+);
+
+CREATE TABLE stores_table (
+store INT PRIMARY KEY,
+size int
+);
+
+CREATE TABLE info_table (
+date date PRIMARY KEY,
+store int,
+Weekly_Sales float,
+IsHoliday boolean
+);
